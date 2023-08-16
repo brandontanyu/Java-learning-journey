ba70b1cb7e16d7095de28625053da1e176cf8cea
Tanyu Brandon <130669586+brandontanyu@users.noreply.github.com>
2023-08-16 15:47:59 +0100

    added folder

---
+++ CodeMagnetChapterOne/puzzle/src/PoolPuzzleOne.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 4){
+            System.out.print("a");
+            if (x < 1){
+                System.out.print(" ");
+            }
+            x += 2;
+            if (x > 0){
+                System.out.print("n");
+                x -= 1;
+            }
+
+            if (x == 1){
+                System.out.print("oise");
+            }
+            if (x)
+
+        }
+
+    }
+}
